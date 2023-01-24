307dc8b26aa464528582459ca170f77b39201725
Kalpesh Patel <1988.kalpesh@gmail.com>
2023-01-23 23:32:06 -0400

    Refactor DatabaseHelperTest

--- shared/src/commonTest/kotlin/com/tarkalabs/expensetracker/data/DatabaseHelperTest.kt
+++ shared/src/commonTest/kotlin/com/tarkalabs/expensetracker/data/DatabaseHelperTest.kt
@@ -2,18 +2,13 @@ package com.tarkalabs.expensetracker.data
 
 import app.cash.turbine.test
 import com.tarkalabs.expensetracker.domain.Category.DINE_OUT
-import com.tarkalabs.expensetracker.domain.UUID
+import com.tarkalabs.expensetracker.ext.toEpochMillis
+import com.tarkalabs.expensetracker.ext.toLocalDate
 import com.tarkalabs.expensetracker.testDbConnection
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
-import kotlinx.datetime.Clock
 import kotlinx.datetime.Clock.System
-import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.atStartOfDayIn
-import kotlinx.datetime.toLocalDateTime
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -21,21 +16,23 @@ import kotlin.test.assertEquals
 class DatabaseHelperTest {
   private lateinit var dbHelper: DatabaseHelper
 
-  @OptIn(ExperimentalCoroutinesApi::class) @BeforeTest fun setup() = runTest {
-    dbHelper = DatabaseHelper(
-      testDbConnection(), Dispatchers.Default
-    )
+  @BeforeTest
+  fun setup() {
+    runBlocking {
+      dbHelper = DatabaseHelper(
+        testDbConnection(), Dispatchers.Default
+      )
+      dbHelper.deleteAll()
+    }
   }
 
   @Test fun `Add Expenses Success`() = runTest {
     dbHelper.addExpense(
       "id", 100.0f, DINE_OUT.toString(),
-      System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date.atStartOfDayIn(
-        TimeZone.currentSystemDefault()
-      ).toEpochMilliseconds(), null
+      System.now().toLocalDate().toEpochMillis(), null
     )
     dbHelper.getAllExpenses().test {
-      assertEquals("id", awaitItem().first().id)
+      assertEquals("id", awaitItem().last().id)
     }
   }
 }
